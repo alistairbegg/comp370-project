a6aacfd246ce394461420787cacb60200ae2fb46
*Siya <*panditsiya0530@gmail.com*>
2025-11-26 01:06:04 +0000

    edited data collection script

--- data_collection.py
+++ data_collection.py
@@ -2,18 +2,37 @@ import http.client
 import urllib.parse
 import json
 import time
+import os
 
+# define key and search terms 
 API_KEY = "PXvKQ6bbIFJXfiwWkGen7UDtwgX2AhTMRq5lnMvl"
 KEY_WORD = "Zohran Mamdani"
 
-article_list = []
+# open previously collected articles 
+if os.path.exists("articles.json"):
+    with open("articles.json", 'r', encoding='utf-8') as f:
+        article_list = json.load(f)
+else: # create empty list for articles
+    article_list = []
 
-while len(article_list) < 100:
+# open previously collected article IDs
+if os.path.exists("article_ids.json"):
+    with open("article_ids.json", 'r', encoding='utf-8') as f:
+        id_list = json.load(f)
+else: # create empty list for article IDs 
+    id_list = []
+
+page = 1
+requests = 0
+
+while len(article_list) < 500 and requests < 100:
     params = urllib.parse.urlencode({
         'api_token': API_KEY,
         'search': KEY_WORD,
-        'limit': 100,
+        'limit': 3,
         'locale': "us,ca,mx",
+        'language': 'en',
+        'page' : page,
     })
 
     # open a secure connection to the news api
@@ -29,12 +48,21 @@ while len(article_list) < 100:
     response_json = json.loads(data)
 
     articles = response_json.get("data", [])
+    
+    for article in articles:
+        article_id = article.get("uuid")
+        if article_id not in id_list:
+            id_list.append(article_id)
+            article_list.append(article)
 
-    article_list.extend(articles)
-
+    page = page+1
+    requests = requests+1
     time.sleep(0.5)
 
-# save to json file
-with open("zohran_mamdani_articles.json", 'w', encoding = 'utf-8') as f:
+# save articles and article ids to json files
+with open("articles.json", 'w', encoding = 'utf-8') as f:
     json.dump(article_list, f, indent=2)
+
+with open("article_ids.json", 'w', encoding='utf-8') as f:
+    json.dump(id_list, f, indent=2)
 
