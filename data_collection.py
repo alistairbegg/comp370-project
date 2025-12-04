d04109a83c444a1b953d61759696724620a2ee6a
*Siya <*panditsiya0530@gmail.com*>
2025-12-04 23:04:02 +0000

    500 articles

--- data_collection.py
+++ data_collection.py
@@ -25,9 +25,9 @@ else: # create empty list for article IDs
 # set other variables 
 page = 1
 requests = 0
-domain = "foxbusiness.com" 
+domain = "theblaze.com" 
 
-while len(articles) < 500 and requests < 6:
+while len(articles) < 532 and requests < 6:
     
     # set parameters 
     params = urllib.parse.urlencode({
